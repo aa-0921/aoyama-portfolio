626b9e3f8ccaee275331ec9a34505f5fd536b2e4
akuto aoyama <aaaa.akuto@gmail.com>
2020-09-18 21:58:41 +0900

    [add]tailwindの追加

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+// module.exports = {
+//   future: {
+//     // removeDeprecatedGapUtilities: true,
+//     // purgeLayersByDefault: true,
+//   },
+//   purge: [],
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {},
+//   plugins: [],
+// }
+module.exports = {
+  purge: ["./components/**/*.jsx", "./pages/**/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
