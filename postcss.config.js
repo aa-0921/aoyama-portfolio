49f2fada1b03a330656df7f1f556a73fbd3d8b65
akuto aoyama <aaaa.akuto@gmail.com>
2020-09-18 22:53:55 +0900

    [add]postcss.config.jsの変更

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,20 @@
+// module.exports = {
+//   plugins: ["tailwindcss", "postcss-preset-env"],
+// };
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./components/**/*.jsx", "./pages/**/*.jsx"],
+  defaultExtractor: (content) => {
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+    return broadMatches.concat(innerMatches);
+  },
+});
+
 module.exports = {
-  plugins: ["tailwindcss", "postcss-preset-env"],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
 };
