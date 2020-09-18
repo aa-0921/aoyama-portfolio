7fe28128befde66fa13a60ea89f341bf2eade27b
akuto aoyama <aaaa.akuto@gmail.com>
2020-09-19 00:35:14 +0900

    [add]tailwind.cssの表示成功

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/global.css";
 import "../styles.css";
+import "../styles/main.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,22 @@
-// module.exports = {
-//   plugins: ["tailwindcss", "postcss-preset-env"],
-// };
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === "production"
-      ? {
-          "@fullhuman/postcss-purgecss": {
-            content: ["./components/**/*.js", "./pages/*.js"],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        }
-      : {}),
-  },
+  plugins: ["tailwindcss", "postcss-preset-env"],
 };
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//     ...(process.env.NODE_ENV === "production"
+//       ? {
+//           "@fullhuman/postcss-purgecss": {
+//             content: ["./components/**/*.js", "./pages/*.js"],
+//             defaultExtractor: (content) =>
+//               content.match(/[\w-/:]+(?<!:)/g) || [],
+//           },
+//         }
+//       : {}),
+//   },
+// };
+
+// module.exports = {
+//   plugins: [require("tailwindcss"), require("autoprefixer")],
+// };
