6c90f1108f341d8d071cae111070c3767b6c9708
akuto aoyama <aaaa.akuto@gmail.com>
2020-09-20 00:57:59 +0900

    [add]アプリ説明文の修正

--- pages/section.js
+++ pages/section.js
@@ -46,32 +46,52 @@ export default function section() {
                         size="lg"
                         color="black"
                       /> */}
-                      <a
-                        href="https://sugomori.herokuapp.com/feed"
-                        className="sugomori-image-wrap"
-                      >
-                        <img src="/images/portfolio/sugomori_index.png" />
-                      </a>
-                      <a
-                        href="https://sugomori.herokuapp.com/"
-                        className="mt-20"
-                      >
-                        <img
-                          src="/images/portfolio/sugomori_about.png"
-                          className="mt-2"
-                        />
-                      </a>
-                      <h3 className="mt-2 text-xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-xl sm:leading-10">
-                        アプリへのリンク↓
-                      </h3>
-                      <h1 className="mt-2 text-xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-base sm:leading-10">
-                        （テストユーザーで単ログインができます）
-                      </h1>
-                      <p className="mt-2 text-base leading-6 text-gray-500">
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Maiores impedit perferendis suscipit eaque, iste
-                        dolor cupiditate blanditiis ratione.
-                      </p>
+                      <div class="flex flex-row">
+                        <div class="flex flex-col">
+                          <a
+                            href="https://sugomori.herokuapp.com/feed"
+                            className="sugomori-image-wrap"
+                          >
+                            <img src="/images/portfolio/sugomori_index.png" />
+                          </a>
+                          <a
+                            href="https://sugomori.herokuapp.com/"
+                            className="sugomori-image-wrap mt-2"
+                          >
+                            <img src="/images/portfolio/sugomori_about.png" />
+                          </a>
+                        </div>
+                        <div class="flex flex-col ml-10">
+                          <h3 className="mt-2 text-xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-xl sm:leading-10">
+                            ←アプリへのリンク
+                          </h3>
+                          <h1 className="text-xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-base">
+                            テストユーザーでログインできます。
+                          </h1>
+                          <p className="mt-2 text-base leading-6 text-gray-500">
+                            [テストユーザー]
+                          </p>
+                          <p className="mt-2 text-base leading-6 text-gray-500">
+                            email: example-1@example.com <br />
+                            password: ababab
+                          </p>
+                          <p className="mt-2 text-base leading-6 text-gray-500">
+                            アプリ名：SUGOMORI
+                            <br />
+                            家でできる好きなことや好きなものの写真をコメント付きで投稿できるアプリです。
+                            <br />
+                            コロナ禍において、何か社会に役立つものをつくれないかと思い作成しました。
+                            <br />
+                            「家で何しよう」と悩む機会が増える中で、「好きなもの」を共有できるアプリがあれば、家で過ごす事が有意義になるのではないかと思いました。
+                          </p>
+                          <a
+                            href="https://sugomori.herokuapp.com/"
+                            className="sugomori-image-wrap mt-2"
+                          >
+                            https://sugomori.herokuapp.com/
+                          </a>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </li>
