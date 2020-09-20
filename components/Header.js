de103153e0ebc791799a8edcf38280105261b0e5
akuto aoyama <aaaa.akuto@gmail.com>
2020-09-21 00:03:51 +0900

    [add]headerが隠れるように変更

--- components/Header.js
+++ components/Header.js
@@ -2,10 +2,45 @@ import Head from "next/head";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
+import React, { useEffect } from "react";
 
 export const Header = () => {
+  useEffect(() => {
+    (function () {
+      const headerTarget = document.getElementById("header"),
+        height = 56;
+
+      let offset = 0,
+        lastPosition = 0,
+        ticking = false;
+      function onScroll(lastPosition) {
+        if (headerTarget != null) {
+          if (lastPosition > height) {
+            if (lastPosition > offset) {
+              headerTarget.classList.add("head-animation");
+            } else {
+              headerTarget.classList.remove("head-animation");
+            }
+            offset = lastPosition;
+          }
+        }
+      }
+
+      window.addEventListener("scroll", function (e) {
+        lastPosition = window.scrollY;
+        if (!ticking) {
+          window.requestAnimationFrame(function () {
+            onScroll(lastPosition);
+            ticking = false;
+          });
+          ticking = true;
+        }
+      });
+    })();
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header className={styles.header} id="header">
       <div className="box">
         <div className="wave -one"></div>
         <div className="wave -two"></div>
