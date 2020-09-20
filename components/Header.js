f2ed2a4572eeb1bd4b1e3ea1a7eed50f1b3d00d8
akuto aoyama <aaaa.akuto@gmail.com>
2020-09-20 23:54:38 +0900

    [add]headerのCSS変更

--- components/Header.js
+++ components/Header.js
@@ -10,21 +10,19 @@ export const Header = () => {
         <div className="wave -one"></div>
         <div className="wave -two"></div>
         <div className="wave -three"></div>
-        <div className="title">
-          <div>
-            <Link href="/">
-              <a>About</a>
-            </Link>
-            <Link href="/SUGOMORI">
-              <a>SUGOMORI</a>
-            </Link>
-            <Link href="/wave">
-              <a>wave</a>
-            </Link>
-            <Link href="/section">
-              <a>section</a>
-            </Link>
-          </div>
+        <div className="title flex flex-col">
+          <Link href="/">
+            <a>About</a>
+          </Link>
+          <Link href="/SUGOMORI">
+            <a>SUGOMORI</a>
+          </Link>
+          <Link href="/wave">
+            <a>wave</a>
+          </Link>
+          <Link href="/section">
+            <a>section</a>
+          </Link>
         </div>
       </div>
     </header>
