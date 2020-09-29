3240ee137fa08f91595c9d80a9d505f8219f7a2c
akuto aoyama <aaaa.akuto@gmail.com>
2020-09-29 23:38:43 +0900

    [add]SkillBarの作成成功

--- components/SkillBar.js
+++ components/SkillBar.js
@@ -10,47 +10,24 @@ export const SkillBar = () => {
     <>
       <li>
         <div className="flex">
-          <div className="ml-4" id="todo-target">
+          <div className="ml-4 w-full" id="todo-target">
             <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 mb-4">
               Skills
             </h3>
-            <div className="flex flex-row">
-              <div className="flex flex-col">
-                <a
-                  href="https://react-todo-list-pied.vercel.app/"
-                  className="sugomori-image-wrap"
-                >
-                  <div className="img-wave-div">
-                    <span className="img-wave"></span>
-                    <img src="/images/qiita.gif" />
-                  </div>
-                </a>
+
+            <div className="flex flex-col w-full">
+              <div className="antd-progress-wrap relative">
+                <p className="absolute text-white w-64 pl-4">SQL</p>
+                <Progress percent={80} status="active" />
               </div>
-              <div className="flex flex-col ml-10">
-                <h3 className=" portfolio-text mt-2 text-xl leading-8 font-extrabold tracking-tight  sm:text-xl sm:leading-10">
-                  progressbar
-                </h3>
-                <div className="portfolio-subtext">
-                  <p className="mt-2 text-base leading-6 ">
-                    説明説明説明説明説明説明説明説明
-                    <br />
-                    説明説明説明説明説明説明説明説明
-                  </p>
-                </div>
-                <a
-                  href="https://qiita.com/_AoAo_"
-                  className="sugomori-image-wrap mt-2 text-blue-700"
-                >
-                  説明説明説明説明説明説明説明説明
-                </a>
-              </div>
+
+              <div className="flex flex-col ml-10"></div>
             </div>
           </div>
         </div>
       </li>
-      <Progress percent={50} status="active" />
 
-      <div className="flex">
+      {/* <div className="flex">
         <div className="ml-4">
           <div className="flex flex-row">
             <div className="flex flex-col">
@@ -62,7 +39,7 @@ export const SkillBar = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </>
   );
 };
