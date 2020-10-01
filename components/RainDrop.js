d268530f1a9b00788ee36c834c7916ec495771a8
akuto aoyama <aaaa.akuto@gmail.com>
2020-10-02 01:13:02 +0900

    [add]handleToggleの追加

--- components/RainDrop.js
+++ components/RainDrop.js
@@ -2,24 +2,41 @@ import React, { useEffect } from "react";
 
 import $ from "jquery";
 import useScript from "../hooks/useScript";
+import { findDOMNode } from "react-dom";
 
 export const RainDrop = () => {
-  const $ = require("jquery");
-  useScript("./raindrops.js");
-  useEffect(() => {
-    $(function () {
-      $("#rain-drop").raindrops({
-        color: "#999",
-        rippleSpeed: 0.2,
-        canvasHeight: 100,
-      });
-    });
-  }, []);
+  // const $ = require("jquery");
+  // useScript("./raindrops.js");
+  // useEffect(() => {
+  //   var script = window.document.createElement("script");
+  //   script.innerHTML = 'console.log("test")';
+  // }
+  // }, []);
+  // if (process.browser) {
+  //   const script = window.document.createElement("script");
+  //   script.innerHTML = 'console.log("test")';
+  //   document.body.appendChild(script);
+  // }
+  // return <>script</>;
+  // const gentest = () => {
+
+  //   const $ = require("jquery");
+
+  //   $("#rain-drop").raindrops({
+  //     color: "#999",
+  //     rippleSpeed: 0.2,
+  //     canvasHeight: 100,
+  //   });
+  // };
+  // return <div id="rain-drop">{gentest()}</div>;
+  const handleToggle = () => {
+    const el = findDOMNode(refs.toggle);
+    $(el).slideToggle();
+  };
 
   return (
-    <>
-      {/* <script src="raindrops.js"></script> */}
-      <div id="rain-drop"></div>
-    </>
+    <div onClick={handleToggle}>
+      <div>hogehoge</div>
+    </div>
   );
 };
